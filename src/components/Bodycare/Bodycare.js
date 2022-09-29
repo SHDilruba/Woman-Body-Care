2bf0b2016c5409be30a67e2e336ebe51ba7cacd4
unknown <hasinadilruba2021@gmail.com>
2022-09-29 15:57:15 +0200

    font added

--- src/components/Bodycare/Bodycare.js
+++ src/components/Bodycare/Bodycare.js
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faDumbbell} from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react';
 import Programs from '../Programs/Programs';
 import'./Bodycare.css';
 
 const Bodycare = () => {
   const [programs, setPrograms] = useState([]);
+  // const [activities, setActivities] = useState([]);
 
   useEffect( () =>{
     fetch('data.json')
@@ -17,9 +20,9 @@ const Bodycare = () => {
 
   return (
     <div className="main-container  container mb-5">
-      <div className="programs-container pt-5 pe-lg-5">
-         <h1 className='text-warning'>Women Body Care</h1>
-           <h4 className='pb-4'>Our Programs </h4>
+      <div className="programs-container pt-5 pe-lg-5">      
+            <h1 className='text-warning'>  <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon> Women Body Care</h1>
+             <h4 className='pb-4'>Our Programs </h4>
              <div className="row row-cols-1 row-cols-lg-3 g-4">         
               {
                programs.map(program => <Programs program={program} key={program.id} handleAddToCart={handleAddToCart} ></Programs>)
