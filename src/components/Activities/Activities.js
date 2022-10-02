d5be82f5cc27c629a5eaac14d5e6179cd576721a
unknown <hasinadilruba2021@gmail.com>
2022-10-02 19:06:13 +0200

    break buttons added

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -27,16 +27,26 @@ const Activities = (props) => {
                <p>Experience</p>
                <p> 5 yrs</p>
            </div>
-        </div>
+        </div>     
           <div>
-             <h5 className='mt-5 mb-3'>Experience Details</h5>
-               <div className='exercise-time bg-light py-2 ps-3'>Exercise time <span className='ms-5 ps-4'></span>
-                </div>
-                  <div className='br-time bg-light mt-3 py-2 ps-3'>Break time <span className='ms-5 ps-5'></span> 
+            <h5 className='mt-5 mb-3'>Add a Break</h5>
+              <div className='break-time px-1'>
+                 <button onClick={addToCart}>3m</button>
+                 <button onClick={addToCart}>4m</button>
+                 <button onClick={addToCart}>3m</button>
+                 <button onClick={addToCart}>4m</button>
+                 <button onClick={addToCart}>2m</button>
               </div>
           </div>
-      </div> 
-     );
+            <div>
+              <h5 className='mt-5 mb-3'>Experience Details</h5>
+                <div className='exercise-time bg-light py-2 ps-3'>Exercise time <span className='ms-5 ps-4'></span>
+                </div>
+                  <div className='br-time bg-light mt-3 py-2 ps-3'>Break time <span className='ms-5 ps-5'></span> 
+                 </div>
+             </div>
+          </div> 
+       );
   };
 
 export default Activities;
